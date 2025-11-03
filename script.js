8df34567fdbe978a4b2587877f19a289be1b2c67
FARIHARUBY <farihaassist@gmail.com>
2025-11-03 22:08:13 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+  // Hide the form after submission
+  document.getElementById('helpRequestForm').style.display = 'none';
+
+  // Show notification
+  document.getElementById('notification').style.display = 'block';
+}
